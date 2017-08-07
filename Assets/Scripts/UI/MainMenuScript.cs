314c4a1802ad5df7be802c6583d6d6de65dd5f08
Johndel <vjohndel07@yahoo.com>
2017-08-06 19:07:27 -0700

    Added Assets for Visuals
    To be FIXED:
    -lost visuals at warArena Scene
    *Others to be followed
    
    Added:
    -Back Animation for ship selection
    
    WIP:
    -Ship Selection UI

--- Assets/Scripts/UI/MainMenuScript.cs
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -8,11 +8,13 @@ public class MainMenuScript : MonoBehaviour {
     public GameObject quitMenu;
     public GameObject mainButtons;
     public GameObject mapSelect;
+    public GameObject shipSelection;
 
     void Start () {
         quitMenu.GetComponent<GameObject>();
         mainButtons.GetComponent<GameObject>();
         mapSelect.GetComponent<GameObject>();
+        shipSelection.GetComponent<GameObject>();
 
         //Default States
         quitMenu.SetActive(false);
@@ -35,6 +37,7 @@ public class MainMenuScript : MonoBehaviour {
     public void confirmMapPress()
     {
         mapSelect.SetActive(false);
+        shipSelection.SetActive(true);
     }
 
     //Exit Button
@@ -54,5 +57,12 @@ public class MainMenuScript : MonoBehaviour {
     public void ConfirmQuit()
     {
         Application.Quit();
+    }
+
+    //Ship Select
+    public void quitShipSelection ()
+    {
+        shipSelection.SetActive(false);
+        mapSelect.SetActive(true);
     }
 }
