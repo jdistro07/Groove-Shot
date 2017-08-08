ac5f86064b8f2466b2fce209e01c8c01e807c7a0
Johndel <vjohndel07@yahoo.com>
2017-08-08 13:03:24 -0700

    Added Main UI sound effects

--- Assets/Scripts/UI/MainMenuScript.cs
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -10,15 +10,40 @@ public class MainMenuScript : MonoBehaviour {
     public GameObject mapSelect;
     public GameObject shipSelection;
 
+    public AudioClip clickConfirm;
+    public AudioClip clickSpecial;
+    public AudioClip clickCancel;
+
+    public AudioSource audioSource;
+
     void Start () {
         quitMenu.GetComponent<GameObject>();
         mainButtons.GetComponent<GameObject>();
         mapSelect.GetComponent<GameObject>();
         shipSelection.GetComponent<GameObject>();
 
+        //Audio
+        audioSource = GetComponent<AudioSource>();
+
         //Default States
         quitMenu.SetActive(false);
 	}
+
+    //Sound Efects
+    public void ClkSpecial()
+    {
+        audioSource.PlayOneShot(clickSpecial);
+    }
+
+    public void ClkConfirm()
+    {
+        audioSource.PlayOneShot(clickConfirm);
+    }
+
+    public void ClkCancel()
+    {
+        audioSource.PlayOneShot(clickCancel);
+    }
 
     //Single Player
     public void ClickSinglePlayer()
