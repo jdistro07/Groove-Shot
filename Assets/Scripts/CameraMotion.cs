45bc68d207aa7f395da3cbc5298093aac5016468
Johndel <vjohndel07@yahoo.com>
2017-08-08 12:05:21 -0700

    Level Changes
    -Optimized Level
    -Added visual effects

--- Assets/Scripts/CameraMotion.cs
+++ Assets/Scripts/CameraMotion.cs
@@ -6,8 +6,9 @@ public class CameraMotion : MonoBehaviour {
 
     public Transform target;
     public float yOffset = 0f;
+    public float zOffset = 0f;
 
 	void LateUpdate() {
-		transform.position = new Vector3 (target.position.x,target.position.y+yOffset,target.position.z);
+		transform.position = new Vector3 (target.position.x,target.position.y+yOffset,target.position.z+zOffset);
 	}
 }
