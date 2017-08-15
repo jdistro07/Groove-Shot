0f60b419073c483f4e723cb0e15c898b63bda0d2
Johndel <vjohndel07@yahoo.com>
2017-08-14 19:37:19 -0700

    Dynamic Ship Selection FINISHED PROBLEMS ARE PRESENT
    -HGLS has been deleted. Need to obtain a copy.

---
+++ Assets/Scripts/UI/ShipPreview.cs
@@ -0,0 +1,71 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShipPreview : MonoBehaviour {
+
+    public List<GameObject> shipList;
+    private int index = 0;
+
+    public Button btnNext;
+
+    // Use this for initialization
+    void Start() {
+
+        //Load all ship preview prefabs from the directory
+        GameObject[] ships = Resources.LoadAll<GameObject>("shipPreview");
+
+        foreach (GameObject c in ships)
+        {
+            /*
+             * Instantiate ships as GameObjects
+             * */
+            GameObject _ships = Instantiate(c) as GameObject;
+            _ships.transform.SetParent(GameObject.Find("ShipSpawn").transform);
+
+            //Add ships to List
+            shipList.Add(_ships);
+
+            //Disable spawned objects and enable the list instead
+            _ships.SetActive(false);
+            shipList[index].SetActive(true);
+        }
+    }
+
+    public void bntNext()
+    {
+        shipList[index].SetActive(false);
+
+        if (index == shipList.Count-1)
+        {
+            index = 0;
+        }
+        else
+        {
+            index++;
+        }
+
+        shipList[index].SetActive(true);
+        Debug.Log(index);
+    }
+
+    public void bntPrev()
+    {
+        shipList[index].SetActive(false);
+
+        if(index == 0)
+        {
+            index = shipList.Count - 1;
+        }
+        else
+        {
+            index--;
+        }
+        
+        shipList[index].SetActive(true);
+
+        Debug.Log(index);
+    }
+}
