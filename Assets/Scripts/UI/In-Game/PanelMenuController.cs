803e83166e27412928e0050ab32f876bccdb0812
Johndel <vjohndel07@yahoo.com>
2017-08-15 09:48:51 -0700

    Added in-game pause feature

---
+++ Assets/Scripts/UI/In-Game/PanelMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelMenuController : MonoBehaviour {
+
+    public GameObject pausePanel;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pausePanel.SetActive(false);
+        }
+    }
+
+    //When resume is clicked
+    public void onClickResume()
+    {
+        pausePanel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UI/In-Game/menuOverlayController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuOverlayController : MonoBehaviour {
+
+    //for pause
+    public GameObject pausePanel;
+
+    // Use this for initialization
+    void Start () {
+        pausePanel.SetActive(false);
+	}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pausePanel.SetActive(true);
+        }
+    }
+}
