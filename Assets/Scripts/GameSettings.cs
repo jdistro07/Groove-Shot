05b73a854d5d41b8fce025c00754d6e2df726e12
Johndel <vjohndel07@yahoo.com>
2017-08-20 16:48:20 -0700

    WIP In-game ceonfig

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour {
+
+    public float musicVoulme;
+    public float sfxVolume;
+
+    public int resolution_index;
+    public int antiAlias_index;
+
+    public bool bloom;
+    public bool motionBlur;
+    public bool vsync;
+}
---
+++ Assets/Scripts/InGameSettingsManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+using UnityEngine.UI;
+
+/*
+This class is for managing the game settings on startup.
+It is in charge of initializing game setting values for the
+whole game not making a reusable configuration file.
+*/
+
+public class InGameSettingsManager : MonoBehaviour {
+
+    //Scripts
+    private GameSettings gameSettings;
+
+    //Option controls
+    //Audio panel controls
+    public Slider slider_music;
+    public Slider slider_sfx;
+
+    //Visual panel controls
+    public Dropdown dropDown_resolution;
+    public Dropdown dropDown_antiAlias;
+    public Toggle toggle_bloom;
+    public Toggle toggle_motionBlur;
+    public Toggle toggle_vsync;
+
+    //PostProcessing Stack Profile
+    public PostProcessingProfile InGame;
+
+    void OnEnable()
+    {
+
+    }
+}
