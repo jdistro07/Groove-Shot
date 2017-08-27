d5832fd0af05f868883b8858128eaa49d3fc08e7
Johndel <vjohndel07@yahoo.com>
2017-08-27 21:10:39 +0800

    Added Pause Feature Issues: Resrcition of Fire on HGLS doesn't work
    Need work on Shooting.cs script and make it dynamic to all types of
    guns.

---
+++ Assets/Scripts/UI/In-Game/Pause.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Pause : MonoBehaviour {
+
+    private bool isPaused;
+
+    private void OnEnable()
+    {
+        pause();
+    }
+
+    private void OnDisable()
+    {
+        resume();
+    }
+
+    private void pause()
+    {
+        try
+        {
+            var hover = GameObject.FindGameObjectWithTag("Player").GetComponent<HoverControls>();
+            //var fireExp = GameObject.FindGameObjectWithTag("Turret").GetComponentInChildren<ShootingExperimental>(); //Experimental script
+            var fire = GameObject.FindGameObjectWithTag("Turret").GetComponentInChildren<Shooting>();
+
+            //fireExp.enabled = false;
+            fire.enabled = false;
+        }
+        catch (NullReferenceException nre)
+        {
+            Debug.Log("[Pause] Player not present");
+        }
+
+    }
+
+    private void resume()
+    {
+        try
+        {
+            var hover = GameObject.FindGameObjectWithTag("Player").GetComponent<HoverControls>();
+            //var fireExp = GameObject.FindGameObjectWithTag("Turret").GetComponentInChildren<ShootingExperimental>(); //Experimental script
+            var fire = GameObject.FindGameObjectWithTag("Turret").GetComponentInChildren<Shooting>();
+
+            //fireExp.enabled = true;
+            fire.enabled = true;
+        }
+        catch (NullReferenceException nre)
+        {
+            Debug.Log("[Pause] Player not present");
+        }
+    }
+}
