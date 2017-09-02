8303ed638f9f2b60038d88949add79013d7ae921
Johndel <vjohndel07@yahoo.com>
2017-09-02 12:54:26 +0800

    Initial death with sounds completed

---
+++ Assets/Scripts/Combat/destroyed.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyed : MonoBehaviour {
+    public AudioClip[] audio_destroyed;
+    public AudioSource audio_souce;
+    public int soundIndex;
+
+    void Awake()
+    {
+        soundIndex = Random.Range(0,audio_destroyed.Length);
+        audio_souce.PlayOneShot(audio_destroyed[soundIndex]);
+    }
+}
