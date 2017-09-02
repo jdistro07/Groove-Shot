f07a494abc289a0261ca6463300695090a54be3f
Johndel <vjohndel07@yahoo.com>
2017-09-02 21:37:22 +0800

    retrace

--- Assets/Scripts/PMovementScripts/CanonAim.cs
+++ Assets/Scripts/PMovementScripts/CanonAim.cs
@@ -10,12 +10,15 @@ public class CanonAim : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		//mousePos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.z, Camera.main.transform.position.y - transform.position.y));
-		//transform.LookAt (mousePos);
+        //mousePos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.z, Camera.main.transform.position.y - transform.position.y));
+        //transform.LookAt (mousePos);
 
 		Vector3 v3T = Input.mousePosition;
 		v3T.z = Mathf.Abs (Camera.main.transform.position.y - transform.position.y);
-		v3T = Camera.main.ScreenToWorldPoint (v3T);
-		transform.LookAt (v3T);
+        v3T = Camera.main.ScreenToWorldPoint (v3T);
+
+        transform.LookAt(v3T);
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+        
 	}
 }
