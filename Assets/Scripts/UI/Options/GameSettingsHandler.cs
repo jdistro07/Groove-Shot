82b74dbac866291fa7431dce2c6a02ad55e67875
Johndel <vjohndel07@yahoo.com>
2017-09-05 09:34:49 +0800

    DAMMITgit status!

--- Assets/Scripts/UI/Options/GameSettingsHandler.cs
+++ Assets/Scripts/UI/Options/GameSettingsHandler.cs
@@ -1,13 +1,13 @@
 ﻿public class GameSettingsHandler {
-    public float musicVoulme = 1f;
-    public float sfxVolume = 1f;
+    public float musicVoulme;
+    public float sfxVolume;
 
-    public int resolution_index = 1;
-    public int antiAlias_index = 1;
+    public int resolution_index;
+    public int antiAlias_index;
 
-    public bool aaEnabled = true;
-    public bool ambientOcclusion = false;
-    public bool bloom = true;
-    public bool motionBlur = true;
-    public bool vsync = true;
+    public bool aaEnabled;
+    public bool ambientOcclusion;
+    public bool bloom;
+    public bool motionBlur;
+    public bool vsync;
 }
---
+++ Assets/Scripts/UI/Options/SettingsManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour {
+
+    private GameSettingsHandler gamesettings;
+
+    public Toggle ambient_toggle;
+
+    private void Awake()
+    {
+        gamesettings = new GameSettingsHandler();
+        Load_Settings();
+    }
+
+    public void OnToggle_AmbientOclussion(bool enabled)
+    {
+        gamesettings.ambientOcclusion = enabled;
+        Debug.Log("Ambient Occlusion: " + enabled);
+    }
+
+    public void OnClick_Apply()
+    {
+        string apply_config = JsonUtility.ToJson(gamesettings,true);
+        File.WriteAllText(Application.persistentDataPath + "/config.json",apply_config);
+
+        SettingsPreset();
+    }
+
+    public void Load_Settings()
+    {
+        gamesettings = JsonUtility.FromJson<GameSettingsHandler>(File.ReadAllText(Application.persistentDataPath + "/config.json"));
+        SettingsPreset();
+    }
+
+    void SettingsPreset()
+    {
+        ambient_toggle.isOn = gamesettings.ambientOcclusion;
+    }
+
+}
