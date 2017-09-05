d57a3df153574db2c695b0488259f1934ae5f6cf
Johndel <vjohndel07@yahoo.com>
2017-09-05 11:56:11 +0800

    CONFIG:
    Motion blur
    Ambient Occlusion
    Bloom

--- Assets/Scripts/UI/Options/SettingsManager.cs
+++ Assets/Scripts/UI/Options/SettingsManager.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using UnityEngine.UI;
+using UnityEngine.PostProcessing;
 
 public class SettingsManager : MonoBehaviour {
 
     private GameSettingsHandler gamesettings;
 
     public Toggle ambient_toggle;
+    public Toggle motionBlur_toggle;
+    public Toggle bloom_toggle;
+
+    public PostProcessingProfile post_inGame;
+    public PostProcessingProfile post_ui;
 
     private void Awake()
     {
@@ -19,7 +25,16 @@ public class SettingsManager : MonoBehaviour {
     public void OnToggle_AmbientOclussion(bool enabled)
     {
         gamesettings.ambientOcclusion = enabled;
-        Debug.Log("Ambient Occlusion: " + enabled);
+    }
+
+    public void OnToggle_MotionBlur(bool enabled)
+    {
+        gamesettings.motionBlur = enabled;
+    }
+
+    public void OnToggle_Bloom(bool enabled)
+    {
+        gamesettings.bloom = enabled;
     }
 
     public void OnClick_Apply()
@@ -38,7 +53,15 @@ public class SettingsManager : MonoBehaviour {
 
     void SettingsPreset()
     {
-        ambient_toggle.isOn = gamesettings.ambientOcclusion;
+        //In Game
+        post_inGame.ambientOcclusion.enabled = ambient_toggle.isOn = gamesettings.ambientOcclusion;
+        post_inGame.motionBlur.enabled = motionBlur_toggle.isOn = gamesettings.motionBlur;
+        post_inGame.bloom.enabled = bloom_toggle.isOn = gamesettings.bloom;
+
+        //UI
+        post_ui.ambientOcclusion.enabled = ambient_toggle.isOn = gamesettings.ambientOcclusion;
+        post_ui.motionBlur.enabled = motionBlur_toggle.isOn = gamesettings.motionBlur;
+        post_ui.bloom.enabled = bloom_toggle.isOn = gamesettings.bloom;
     }
 
 }
