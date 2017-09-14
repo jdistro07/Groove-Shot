8ad36923af9673a631eefede69c0ee9246629648
Jasper Lance Dayon <jasperlancecdayon@yahoo.com>
2017-09-15 00:34:08 +0800

    added and tested DBLoader Script

---
+++ Assets/JASPER/Lance Workshop/ScriptsExperimental/DBLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DBLoader : MonoBehaviour
+{
+	public string[] users;
+
+	IEnumerator Start()
+	{
+		WWW username = new WWW ("http://localhost/GrooveShotDB/getusers.php");
+		yield return username;
+		string userData = username.text;
+		users = userData.Split(';');
+		print (GetUsername(users[2], "Username: "));
+	}
+
+	string GetUsername(string name, string index)
+	{
+		string username = name.Substring(name.IndexOf(index)+index.Length);
+		return username;
+	}
+}
