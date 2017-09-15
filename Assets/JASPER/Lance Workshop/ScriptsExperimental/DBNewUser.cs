bd8100f64742d4e6850132cdbc68494ba6dbe1a4
Jasper Lance Dayon <jasperlancecdayon@yahoo.com>
2017-09-15 14:10:12 +0800

    Add users to database function added

---
+++ Assets/JASPER/Lance Workshop/ScriptsExperimental/DBNewUser.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DBNewUser : MonoBehaviour
+{
+	string addUsersLink = "http://localhost/GrooveShotDB/addusers.php";
+
+	public Button createButton;
+	public InputField usernameInput;
+
+	void Start()
+	{
+		Button btn = createButton.GetComponent<Button> ();
+		InputField inp = usernameInput.GetComponent<InputField> ();
+		btn.onClick.AddListener (CreateUser);
+	}
+
+	void TaskOnClick()
+	{
+		CreateUser ();
+	}
+
+	public void CreateUser()
+	{
+		string username = usernameInput.text;
+
+		if (usernameInput.text != "")
+		{
+			WWWForm form = new WWWForm ();
+			form.AddField ("usernamePost", username);
+
+			WWW www = new WWW (addUsersLink, form);
+		}
+		else
+		{
+			usernameInput.text = "";
+			usernameInput.placeholder.GetComponent<Text> ().text = "No null usernames";
+		}
+	}
+}
