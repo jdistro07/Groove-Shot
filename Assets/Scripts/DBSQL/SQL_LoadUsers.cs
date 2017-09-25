a9730d967e59d4e571bb27b3bf9342b1cddc3faa
Johndel <vjohndel07@yahoo.com>
2017-09-25 22:44:18 +0800

    Fixed user list loading users problem

--- Assets/Scripts/DBSQL/SQL_LoadUsers.cs
+++ Assets/Scripts/DBSQL/SQL_LoadUsers.cs
@@ -8,6 +8,7 @@ public class SQL_LoadUsers : MonoBehaviour {
     [Header("DATABASE COMPONENTS")]
     public string url = "http://localhost/GrooveShotDB/getusers.php"; // default link
     public string[] users;
+    public bool called = false;
 
     public GameObject button;
     public RectTransform panel;
@@ -16,6 +17,11 @@ public class SQL_LoadUsers : MonoBehaviour {
     private void OnEnable()
     {
         StartCoroutine(LoadUsers());
+    }
+
+    private void OnDisable()
+    {
+        users = new string[0];
     }
 
     private IEnumerator LoadUsers()
@@ -27,12 +33,21 @@ public class SQL_LoadUsers : MonoBehaviour {
         string data = link.text;
         users = data.Split(';');
 
-        for (int count=0; count<users.Length; count++)
+        if (!called)
         {
-            GameObject go_button = (GameObject)Instantiate(button);
-            buttonText.text = users[count];
+            called = true;
+            for (int count = 0; count < users.Length; count++)
+            {
+                GameObject go_button = (GameObject)Instantiate(button);
+                buttonText.text = users[count];
 
-            go_button.transform.SetParent(panel,false);
+                go_button.transform.SetParent(panel, false);
+                yield return count;
+            }
         }
+        else
+        {
+            Debug.Log("[SQL_LoadUsers.cs]Database users already loaded...");
+        } 
     }
 }
